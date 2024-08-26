b104c2bee0b450ef4f5f2e76c119a5e0be68ac41
Senterget <serebrennikov-vasya@mail.ru>
2024-08-27 02:03:48 +0700

    5 commit

--- main.py
+++ main.py
@@ -2,51 +2,35 @@ import os.path
 import os
 
 
-with open("write.txt", encoding='utf-8') as f:
-    ST_TITLE = 1
-    ST_COUNT = 2
-    ST_INGREDIENTS = 3
-    cook_book = {}
-    state = ST_TITLE
-    for line in f:
-        line = line.strip()
-        if not line: continue
-        if state == ST_TITLE:
-            title = line
-            cook_book[title] = []
-            state = ST_COUNT
-        elif state == ST_COUNT:
-            count = int(line)
-            state = ST_INGREDIENTS
-        else: # if state == ST_INGREDIENTS:
-            data = [x.strip() for x in line.split('|')]
-            data[1] = int(data[1])
-            cook_book[title].append(dict(zip(('ingredient_name', 'quantity', 'measure'), data)))
-            count -= 1
-            if count == 0:
-                state = ST_TITLE
+def readfile():
+    with open("write.txt", encoding='utf-8') as f: #создание словаря
+        ST_TITLE = 1
+        ST_COUNT = 2
+        ST_INGREDIENTS = 3
+        cook_book = {}
+        state = ST_TITLE
+        for line in f:
+            line = line.strip()
+            if not line: continue
+            if state == ST_TITLE:
+                title = line
+                cook_book[title] = []
+                state = ST_COUNT
+            elif state == ST_COUNT:
+                count = int(line)
+                state = ST_INGREDIENTS
+            else: # if state == ST_INGREDIENTS:
+                data = [x.strip() for x in line.split('|')]
+                data[1] = int(data[1])
+                cook_book[title].append(dict(zip(('ingredient_name', 'quantity', 'measure'), data)))
+                count -= 1
+                if count == 0:
+                    state = ST_TITLE
 
 
-def get_shop_list_by_dishes(person_count, dishes):
-    for p in cook_book[title]:
+def get_shop_list_by_dishes(person_count, dishes): #функция, которая на вход принимает список блюд из cook_book и количество персон для кого мы будем готовить
+    for p in dictionary(cook_book[title]):
         if p == dishes:
             quantity = quantity * person_count
-            print(result)
-get_shop_list_by_dishes(['Омлет', 'Омлет'], 2)
-
-
-def readfile(path: str):
-    path = 'D:\directory'
-    files_list = os.listdir(path)
-    d = {}
-    for i in range(1, 3):
-        name = f'{i}.txt'
-        with open(name, 'r', encoding='utf-8') as f:
-            d[name] = [x for x in f.read().splitlines() if x]
-
-    with open('the_source_file.txt', 'w', encoding='utf-8') as file:
-        for k, v in sorted(d.items(), key=lambda x: -len([x[1]])):
-            file.write(k + '\n')
-            file.write(str(len(v)) + '\n')
-            file.write('\n'.join(v))
-            file.write('\n')
+            print(dishes)
+    get_shop_list_by_dishes(['Омлет', 'Омлет'], 2)
